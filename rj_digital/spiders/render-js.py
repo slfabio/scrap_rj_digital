f2a100cfc9b0c1b9cae59f0ec8afc683cd2ac66d
Fábio Lima <fabio.sza.lima@gmail.com>
2025-01-15 17:25:00 -0300

    Remove renser-js

--- rj_digital/spiders/render-js.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-import w3lib
-
-class PlaywrightSpider(scrapy.Spider):
-
-    def parse(self, response):
-        from scrapy.http.response.html import HtmlResponse
-        ht = HtmlResponse(url=response.url, body=response.body, encoding="utf-8", request=response.request)
-        return None
