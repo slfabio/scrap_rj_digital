dfbef653e8765ae9d8dea0924c18a085b067d268
Fábio Lima <67182132+slfabio@users.noreply.github.com>
2025-01-15 16:42:17 -0300

    Update render-js.py

--- rj_digital/spiders/render-js.py
+++ rj_digital/spiders/render-js.py
@@ -2,8 +2,6 @@ import scrapy
 import w3lib
 
 class PlaywrightSpider(scrapy.Spider):
-    name = "playwright"
-    start_urls = ["https://resultados.tse.jus.br/oficial/app/index.html#/eleicao;e=e619;uf=rj;mu=58653;tipo=3/resultados/cargo/13"]  # avoid using the default Scrapy downloader
 
     def parse(self, response):
         from scrapy.http.response.html import HtmlResponse
